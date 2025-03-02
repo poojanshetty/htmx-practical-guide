32764cf08e2f774c024dfabb52f7a965f9dbf68a
Pooja Shetty <poojaniranjanshetty@gmail.com>
2025-03-02 20:21:17 +0530

    Use hx-vals to send params to post body

--- 02-02-advanced/views/components/location.js
+++ 02-02-advanced/views/components/location.js
@@ -1,7 +1,10 @@
 export default function renderLocation(location) {
   return `
     <li class="location-item">
-      <button>
+      <button
+        hx-post="/places"
+        hx-vals='{"locationId": "${location.id}"}'
+      >
         <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
         <h3>${location.title}</h3>
       </button>
