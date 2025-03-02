2fd703999dc7d7685c862b17e7da4a44b3e7855f
Pooja Shetty <poojaniranjanshetty@gmail.com>
2025-03-02 12:13:16 +0530

    Use inheritance on parent tag for hx-swap in order to prevent code duplication

--- 02-advanced/app.js
+++ 02-advanced/app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
           </form>
         </section>
         <section>
-          <ul id="goals">
+          <ul id="goals" hx-swap="outerHTML">
           ${courseGoals.map(
             (goal) => `
             <li id="goal-${goal.id}">
@@ -43,7 +43,6 @@ app.get('/', (req, res) => {
               <button
                 hx-delete="/goals/${goal.id}"
                 hx-target="#goal-${goal.id}"
-                hx-swap="outerHTML"
               >
                 Remove
               </button>
@@ -70,7 +69,6 @@ app.post('/goals', (req, res) => {
       <button
         hx-delete="/goals/${id}"
         hx-target="#goal-${id}"
-        hx-swap="outerHTML"
       >
         Remove
       </button>
