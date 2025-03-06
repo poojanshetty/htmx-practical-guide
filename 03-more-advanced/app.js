6734071cf546b3372e2b91161936486752d1c8b4
Pooja Shetty <poojaniranjanshetty@gmail.com>
2025-03-06 11:45:52 +0530

    Use HX-Retarget and HX-Reswap to simulate handling in client side (eqvuivalent to hx-targer and hx-swap on server side)

--- 03-more-advanced/app.js
+++ 03-more-advanced/app.js
@@ -112,6 +112,15 @@ app.post('/login', (req, res) => {
       </ul>
     `);
   }
+
+  if (Math.random() > 0.5) {
+    res.setHeader("HX-Retarget", ".control");
+    res.setHeader("HX-Reswap", "beforebegin");
+    return res.send(`
+      <p class="error">A server side error has occured. Please try again.</p>
+    `)
+  }
+
   res.setHeader("HX-Redirect", "/authenticated");
   res.send();
 });
